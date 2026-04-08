230e5f294cfff2ba9b0b6105be3433c4709b9837
lowkeyfish <hnyujunyang@gmail.com>
2026-04-08 18:10:35 +0800

    update copyright.

--- vertx-template-common/src/main/java/com/yujunyang/vertx/template/common/enums/ValueEnum.java
+++ vertx-template-common/src/main/java/com/yujunyang/vertx/template/common/enums/ValueEnum.java
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Yu Junyang (https://github.com/lowkeyfish)
+ * SPDX-License-Identifier: MIT
+ */
+
 package com.yujunyang.vertx.template.common.enums;
 
 /** 具有值枚举类型 */
--- vertx-template-common/src/main/java/com/yujunyang/vertx/template/common/log4j2/OtelSpanContextDataProvider.java
+++ vertx-template-common/src/main/java/com/yujunyang/vertx/template/common/log4j2/OtelSpanContextDataProvider.java
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Yu Junyang (https://github.com/lowkeyfish)
+ * SPDX-License-Identifier: MIT
+ */
+
 package com.yujunyang.vertx.template.common.log4j2;
 
 import io.opentelemetry.api.trace.Span;
