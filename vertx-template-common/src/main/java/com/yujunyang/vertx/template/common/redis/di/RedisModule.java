287bcbf6b3f1492552682492e444379a80010694
lowkeyfish <hnyujunyang@gmail.com>
2026-04-30 23:50:27 +0800

    update

--- vertx-template-common/src/main/java/com/yujunyang/vertx/template/common/redis/di/RedisModule.java
+++ vertx-template-common/src/main/java/com/yujunyang/vertx/template/common/redis/di/RedisModule.java
@@ -9,6 +9,10 @@ import com.yujunyang.vertx.template.common.config.ApplicationConfigProvider;
 import com.yujunyang.vertx.template.common.config.RedisConfig;
 import dagger.Module;
 import dagger.Provides;
+import io.vertx.core.Vertx;
+import io.vertx.redis.client.Redis;
+import io.vertx.redis.client.RedisAPI;
+import io.vertx.redis.client.RedisOptions;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
@@ -23,5 +27,21 @@ public class RedisModule {
         config.useSingleServer().setAddress(redisConfig.getAddress());
         RedissonClient redissonClient = Redisson.create(config);
         return redissonClient;
+    }
+
+    @Provides
+    public Redis provideRedis(Vertx vertx, ApplicationConfigProvider applicationConfigProvider) {
+        RedisConfig redisConfig = applicationConfigProvider.getConfig().getRedis();
+        RedisOptions options = new RedisOptions()
+                .setConnectionString(redisConfig.getAddress())
+                .setPassword(redisConfig.getPassword());
+
+        Redis redis = Redis.createClient(vertx, options);
+        return redis;
+    }
+
+    @Provides
+    public RedisAPI provideRedisAPI(Redis redis) {
+        return RedisAPI.api(redis);
     }
 }
